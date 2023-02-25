03823ebf4442c0773f2bcf26f2c0958ef9d994c9
vivekjais1110 <ayushjais86047@gmail.com>
2023-02-25 19:16:57 +0530

    changes dep/anxty connect mongodb and name slot

--- Anxiety/Week1/actions/actions.py
+++ Anxiety/Week1/actions/actions.py
@@ -11,6 +11,7 @@ import logging
 import json
 from datetime import datetime
 from typing import Any, Dict, List, Text, Optional
+import pymongo
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.types import DomainDict
@@ -39,6 +40,80 @@ Returns:
 Returns:
     Slot to None
 """
+
+def connection():
+    try:
+        conn_str = 'mongodb+srv://dev_admin:mpB1zujXTI1DdJxS@psybot1.14npa.mongodb.net/prod_psybot1?authSource=admin&retryWrites=true&w=majority'
+        db = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
+        # collection = db.iwill.bot_emotion
+        # print("...........",db.server_info())
+        print("connected...........")
+    except Exception:
+        print("Unable to connect to the server................")
+    return db
+
+class ActionSaveName(Action):
+    
+    def name(self) -> Text:
+        return "action_save_name"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        name = tracker.get_slot("name")
+        bot_session_id = tracker.sender_id
+        print("bot_session_id", bot_session_id)
+        if name:
+            print("name_slot",name) 
+            conn = connection()
+            result = conn.iwill.bot_username.insert_one(
+                {
+                    "bot_session_id": bot_session_id,
+                    "username": name
+                }
+            )
+            print(result)
+            print(result.inserted_id)
+            result = conn.iwill.bot_username.find(
+            {
+                "bot_session_id": bot_session_id
+            }
+            )
+            names = []
+            for doc in result:
+                print(doc['username']) 
+                names.append(doc['username'])
+            print(names)
+            conn.close()
+        #dispatcher.utter_message(template="utter_guided_trymore") 
+        return [SlotSet("name", name)] 
+
+class ActionGetName(Action):
+    
+    def name(self) -> Text:
+        return "action_get_name"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        name = tracker.get_slot("name")
+        bot_session_id = tracker.sender_id
+        print("bot_session_id", bot_session_id)
+        print("name_slot",name) 
+        conn = connection()
+        result = conn.iwill.bot_username.find(
+            {
+                "bot_session_id": bot_session_id
+            }
+        )
+        names = []
+        for doc in result:
+            print(doc['username']) 
+            names.append(doc['username'])
+        print(names)
+        name = names[0]
+        return [SlotSet("name", name)]
+    
 class ActionSetInput7(Action):
 
     def name(self) -> Text:
